ef770807e5e2f76bfcda2d3f9b2a916efc9da4fd
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-04-23 03:23:20 +0300

    add cxx and core headers with basic using entities

---
+++ include/son8/cxx.hxx
@@ -0,0 +1,12 @@
+#ifndef SON8_CXX_HXX
+#define SON8_CXX_HXX
+
+#include <son8/cxx/core.hxx>
+
+#endif//SON8_CXX_HXX
+
+// Apache License 2.0
+// NO WARRANTY OF ANY KIND see <http://www.apache.org/licenses/LICENSE-2.0>
+// SPDX-License-Identifier: Apache-2.0
+// lib: cxx_unit C++17 Standard CXX Entities Namespace
+// Ⓒ Copyright (c) [YEAR] Oleg'Ease'Kharchuk ᦒ
---
+++ include/son8/cxx/core.hxx
@@ -0,0 +1,67 @@
+#ifndef SON8_CXX_CORE_HXX
+#define SON8_CXX_CORE_HXX
+
+#include <array>
+#include <initializer_list>
+#include <limits>
+#include <new>
+#include <tuple>
+#include <utility>
+
+namespace son8::cxx {
+    // C++03
+    // -- _many_
+    using std::swap;
+    // -- limits
+    using std::float_denorm_style;
+    using std::float_round_style;
+    using std::numeric_limits;
+    // -- utility
+    using std::make_pair;
+    using std::pair;
+    // C++11
+    // -- _many_
+    using std::begin;
+    using std::end;
+    using std::get;
+    using std::ignore;
+    using std::tuple_element;
+    using std::tuple_size;
+    // -- array
+    using std::array;
+    // -- initializer_list
+    using std::initializer_list;
+    // -- tuple
+    using std::make_tuple;
+    using std::tuple;
+    using std::tie;
+    // -- utility
+    using std::forward;
+    using std::move;
+    // C++14
+    // -- _many_
+    using std::crbegin;
+    using std::crend;
+    using std::rbegin;
+    using std::rend;
+    // -- utility
+    using std::exchange;
+    // C++17
+    // -- _many_
+    using std::data;
+    using std::empty;
+    using std::size;
+    // -- new
+    using std::launder;
+    // -- utility
+    using std::as_const;
+
+} // namespace son8
+
+#endif//SON8_CXX_CORE_HXX
+
+// Apache License 2.0
+// NO WARRANTY OF ANY KIND see <http://www.apache.org/licenses/LICENSE-2.0>
+// SPDX-License-Identifier: Apache-2.0
+// lib: cxx_unit C++17 Standard CXX Entities Namespace
+// Ⓒ Copyright (c) [YEAR] Oleg'Ease'Kharchuk ᦒ
